915cc03c2b2ea6824aa0e161049c1fc3561dcc59
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-09-18 15:20:40 +0100

    Notification service

---
+++ Solution1/1 - Single Responsibility Principle (SRP)/Notification Service/Notification-Service.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single_Responsibility_Principle_SRP.Notification_Service
+{
+
+    public class NotificationService
+    {
+        public enum enNotificationType { Email, SMS, Fax }
+
+        public void SendNotification(string to, string message, enNotificationType NotificationType)
+        {
+
+            if (NotificationType == enNotificationType.Email)
+            {
+                EmailService.SendEmail(to, message);
+            }
+            else if (NotificationType == enNotificationType.SMS)
+            {
+                SMSService.SendSMS(to, message);
+            }
+            else if (NotificationType == enNotificationType.Fax)
+            {
+                FaxService.SendFax(to, message);
+            }
+        }
+
+    }
+
+    public class EmailService
+    {
+        // Method to send email
+        public static void SendEmail(string to, string message)
+        {
+            Console.WriteLine($"\nSending Email to {to}: {message}");
+        }
+    }
+
+    public class SMSService
+    {
+        // Method to send SMS
+        public static void SendSMS(string to, string message)
+        {
+            Console.WriteLine($"\nSending SMS to {to}: {message}");
+        }
+    }
+
+
+
+    public class FaxService
+    {
+        // Method to send Fax
+        public static void SendFax(string to, string message)
+        {
+            Console.WriteLine($"\nSending Fax to {to}: {message}");
+        }
+    }
+
+
+    public class NotificationDemo
+    {
+        public static void RunNotificationService()
+        {
+            // Create an instance of the NotificationService
+            NotificationService notificationService = new NotificationService();
+
+            // Send an email
+            notificationService.SendNotification("john@example.com", "Welcome to our service!", NotificationService.enNotificationType.Email);
+
+            // Send an SMS
+            notificationService.SendNotification("+123456789", "Your OTP code is 1234.", NotificationService.enNotificationType.SMS);
+
+            // Send a fax
+            notificationService.SendNotification("123-456-789", "Fax Message: Important document.", NotificationService.enNotificationType.Fax);
+
+        }
+    }
+
+}
---
+++ Solution1/1 - Single Responsibility Principle (SRP)/Program.cs
@@ -0,0 +1,43 @@
+﻿using Single_Responsibility_Principle_SRP.Notification_Service;
+
+namespace _1___Single_Responsibility_Principle__SRP_
+{
+
+    /*
+     * 
+     * Introduction to SOLID principles
+        SOLID principles are the design principles that enable us to manage several software design problems.
+        Robert C. Martin compiled these principles in the 1990s. 
+        These principles provide us with ways to move from tightly coupled code and little encapsulation to the desired results of loosely coupled and encapsulated real business needs properly. SOLID is an acronym for the following.
+
+        S: Single Responsibility Principle (SRP)
+        O: Open-closed Principle (OCP)
+        L: Liskov substitution Principle (LSP)
+        I: Interface Segregation Principle (ISP)
+        D: Dependency Inversion Principle (DIP)
+     * 
+     * 
+     */
+
+
+
+    internal class Program
+    {
+
+
+        // ***** [  S: Single Responsibility Principle(SRP) ] *******
+
+
+        // => SRP says, "Every software module should have only one reason to change." meaning it should have only one job or responsibility.
+
+
+        static void Main(string[] args)
+        {
+            NotificationDemo.RunNotificationService();
+
+
+
+        }
+
+    }
+}
